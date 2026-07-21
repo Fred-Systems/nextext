1cf7c12becd0e578af5b14763082725638e3ca23
Fred Systems <fred@nextext.app>
2026-07-20 23:48:36 -0400

    Fix mobile white screen and expand splashscreen layout

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // CRITICAL: Forces relative pathing so your phone can find the files locally
   plugins: [react()],
 })
+
